3234e14bd4011d59deb4cc9e9d03d8978cb60ad2
AayushBhagat256 <113020296+AayushBhagat256@users.noreply.github.com>
2022-11-09 19:51:47 +0530

    Navbar ready

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> Synapse Website
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Synapse OP
-          <p>I am Aayush</p>
-        </a>
-      </header>
+    <div>
+      <div className="all">
+         <Navbar/>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
