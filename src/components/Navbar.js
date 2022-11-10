4004bc521395e3a8e8572b99a3200ad54474252c
AayushBhagat256 <113020296+AayushBhagat256@users.noreply.github.com>
2022-11-10 19:10:54 +0530

    Navbar&Bg Ready

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,25 +4,34 @@ import './Navbar.css'
 
 function Navbar() {
   return (
-    <div className='cuscolor'>
+    <div className='cuscolor mx-1'>
       <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="/">
-      <img src={Logo} alt="Logo" width="60" height="50" class="d-inline-block align-text-top"/>
-      <span class="navbar-brand mb-1 h1"> Synapse</span>
-    </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" href="/">Link</a>
-        </li>
-        {/* <li class="nav-item dropdown">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="/">
+            <img src={Logo} alt="Logo" width="80" height="80" class="d-inline-block align-text-top" />
+            {/* <span class="navbar-brand mb-1 h1"> Synapse</span> */}
+          </a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active mx-2" aria-current="page" href="/">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active mx-2" href="/">Projects</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active mx-2" aria-current="page" href="/">Events</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active mx-2" aria-current="page" href="/">Resources</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active mx-2" aria-current="page" href="/">Contact Us</a>
+              </li>
+              {/* <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Dropdown
           </a>
@@ -36,14 +45,14 @@ function Navbar() {
         <li class="nav-item">
           <a class="nav-link disabled">Disabled</a>
         </li> */}
-      </ul>
-      {/* <form class="d-flex" role="search">
+            </ul>
+            {/* <form class="d-flex" role="search">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
         <button class="btn btn-outline-success" type="submit">Search</button>
       </form> */}
-    </div>
-  </div>
-</nav>
+          </div>
+        </div>
+      </nav>
     </div>
   )
 }
