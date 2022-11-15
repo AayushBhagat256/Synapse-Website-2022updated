bf7d7c6468cdc4529b48e0d6ec61c8c9306206b0
AayushBhagat256 <113020296+AayushBhagat256@users.noreply.github.com>
2022-11-15 22:25:27 +0530

    API is working :)

--- src/components/Faculty.js
+++ src/components/Faculty.js
@@ -2,21 +2,23 @@ import React, { useEffect } from 'react'
 import './Faculty.css'
 import { useState } from 'react'
 //import Logo from './synapselogo.jpg'
-import IAF from './IAF.png'
+//import IAF from './IAF.png'
 
 function Faculty() {
     const [display, setDisplay] = useState([]);
     const fetchApi = () => {
-        fetch("https://jsonplaceholder.typicode.com/users")
+        fetch("https://synapseop.pythonanywhere.com/faculty/")
             .then((data) => {
                 console.log(data)
                 return data.json();
             })
             .then((objectData) => {
                 console.log(objectData)
-                setDisplay(objectData)
+                console.log(objectData.payload)
+                setDisplay(objectData.payload)
             })
     }
+    let imgurl = "https://synapseop.pythonanywhere.com"
 
     useEffect(
         () => {
@@ -29,42 +31,26 @@ function Faculty() {
         <div className='faculty'>
             <h1 className='heading'>This is faculty </h1>
 
-            {/* {display.map(map =>
-                <div className="col-md-4 ">
-                    <div class="card" style={{ width: "18rem;" }}>
-                        <img src="/" class="card-img-top" alt="" />
-                        <div class="card-body">
-                            <h5 class="card-title">{map.name}</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="/" class="btn btn-primary">{map.email}</a>
-                        </div>
-                    </div>
-                </div>
-            )} */}
+
             <div className="container">
                 <h1 className='hello1'>hello</h1>
                 <div className="row">
-                    {display.map(map=>
-                    <div className="col-md-4">
-                    {/* <h1>hello</h1> */}
-                    <div class="card my-1" style={{ width: "18rem;" }}>
-                <img src={IAF} class="card-img-top" alt="" />
-                <div class="card-body">
-                    <h5 class="card-title">{map.name}</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="/" class="btn btn-primary">{map.email}</a>
-                </div>
-            </div>
-                </div>
+                    {display.map(map =>
+                        <div className="col-md-4" key={map.id}>
+                            {/* <h1>hello</h1> */}
+                            <div class="card my-1" style={{ width: "18rem" }}>
+                                <img src={imgurl + map.Profilepic} class="card-img-top" alt="" />
+                                <div class="card-body">
+                                    <h5 class="card-title">{map.Fname} {map.Lname}</h5>
+                                    <p class="card-text">{map.Designation} <br />{map.testimonial} <br />{map.fk_contactid.github}</p>
+                                    <a href="/" class="btn btn-primary">Button</a>
+                                </div>
+                            </div>
+                        </div>
                     )
-                    
-}
-                    {/* <div className="col-md-4">
-                        <h1>hello</h1>
-                    </div>
-                    <div className="col-md-4">
-                        <h1>hello</h1>
-                    </div> */}
+
+                    }
+
                 </div>
             </div>
 
