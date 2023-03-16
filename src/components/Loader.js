9ae7b410072901641bbdd11d5b0d9d204ec36fc3
AayushBhagat256 <113020296+AayushBhagat256@users.noreply.github.com>
2023-03-16 12:01:28 +0530

    ProjectsApiIntegrated

---
+++ src/components/Loader.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Loader.css'
+
+function Loader() {
+  return (
+    <div className='basic'>
+      <p>Loading please wait</p>
+    </div>
+  )
+}
+
+export default Loader
--- src/components/Project2.js
+++ src/components/Project2.js
@@ -3,63 +3,105 @@ import * as React from 'react'
 import './Project2.css'
 import { Box, CardHeader } from '@chakra-ui/react'
 import {
-    Card, CardBody, CardFooter,
-    SimpleGrid, Heading, Text, Button
+  Card, CardBody, CardFooter,
+  SimpleGrid, Heading, Text, Button
 } from '@chakra-ui/react'
 import {
-    Modal,
-    ModalOverlay,
-    ModalContent,
-    ModalHeader,
-    ModalFooter,
-    ModalBody,
-    ModalCloseButton,
-    useDisclosure
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure
 } from '@chakra-ui/react'
+import Aos from 'aos';
+import 'aos/dist/aos.css'
+import { useState,useEffect } from 'react'
+import Loader from './Loader'
 
 
 function Project2() {
-    const { isOpen, onOpen, onClose } = useDisclosure()
-    return (
-        <div>
-            <SimpleGrid spacing={4} templateColumns='repeat(auto-fill, minmax(200px, 1fr))'>
-                <Card className='project2card' style={{background:'transparent',background: 'rgba(84, 84, 142, 0.25)',boxShadow: '0 8px 32px 0 rgba(91, 38, 135, 0.37)',borderRadius: '2em',border: '1px solid rgba(97, 149, 187, 0.555)',overflow:'hidden',WebkitBackdropFilter:'blur(1px)',backdropFilter:'blur(1px)',color: '#81C6E8'}}>
-                    <CardHeader className='cardheader' >
-                        <Heading size='md' style={{color: '#81C6E8'}}> Customer dashboard</Heading>
-                    </CardHeader>
-                    <CardBody>
-                        <Text>View a summary of all your customers over the last month.</Text>
-                    </CardBody>
-                    <CardFooter>
-                        <Button onClick={onOpen} style={{background:'transparent',border: '1px solid rgba(97, 149, 187, 0.555)',borderRadius: '2em'}}>View here</Button>
-                    </CardFooter>
-                    <Modal blockScrollOnMount={false} isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Modal Title</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-            <Text fontWeight='bold' mb='1rem'>
-              You can scroll the content behind the modal
-            </Text>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt dolorem accusamus excepturi.
-          </ModalBody>
+  useEffect(
+    ()=>{
+        Aos.init({duration:2000});
+    },[]
+)
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const [loading,setLoading] = useState(true)
+  const [projects, setProjects] = useState([]);
+  const fetchApi = () => {
+    fetch("https://synapseop.pythonanywhere.com/projects/")
+      .then((data) => {
+        console.log(data)
+        return data.json();
+      })
+      .then((objectData) => {
+        console.log(objectData)
+        console.log(objectData.payload)
+        setProjects(objectData.payload)
+        setLoading(false)
+      })
+  }
 
-          <ModalFooter>
-            <Button colorScheme='blue' mr={3} onClick={onClose}>
-              Close
-            </Button>
-            <Button variant='ghost'>Secondary Action</Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
-                        
-                </Card>
+  useEffect(
+    () => {
+      fetchApi();
+    }, []
+  )
+  return (
+    <div>
+      <Heading size='md' style={{textAlign:'center'}}>Here Are few projects...</Heading>
+      <br />
+      <SimpleGrid spacing={4} columns={{ base: 1, md: 2, md: 3 }} style={{padding:'6px'}}>
+        {
+          loading?(<Loader/>):(
+            projects.map((map)=>(
+              <Card className='project2card  'data-aos="fade-up" style={{ background: 'transparent', background: 'rgba(84, 84, 142, 0.25)', boxShadow: '0 8px 32px 0 rgba(91, 38, 135, 0.37)', borderRadius: '2em', border: '1px solid rgba(97, 149, 187, 0.555)', overflow: 'hidden', WebkitBackdropFilter: 'blur(1px)', backdropFilter: 'blur(1px)', color: '#81C6E8' }}>
+            <CardHeader className='cardheader' >
+              <Heading size='md' style={{ color: '#81C6E8' }}>{map.title}</Heading>
+            </CardHeader>
+            <CardBody>
+              <Text>{map.field}</Text>
+              <br />
+              <Text>{map.description.slice(0,390)+'...'}</Text>
+            </CardBody>
+            <CardFooter>
+              <a href={map.link}><Button  style={{ background: 'transparent', border: '1px solid rgba(97, 149, 187, 0.555)', borderRadius: '2em',textDecoration:'none' }}>Read more</Button></a>
+              
+            </CardFooter>
+            {/* <Modal blockScrollOnMount={false} isOpen={isOpen} onClose={onClose}>
+              <ModalOverlay />
+              <ModalContent>
+                <ModalHeader>Modal Title</ModalHeader>
+                <ModalCloseButton />
+                <ModalBody pb={12}>
+                  <Text fontWeight='bold' mb='1rem'>
+                  {map.description}
+                  </Text>
+                </ModalBody>
+  
+                <ModalFooter>
+                  <Button colorScheme='blue' mr={3} onClick={onClose}>
+                    Close
+                  </Button>
+                  <a href={map.link}><Button variant='ghost'>Download</Button></a>
+                  
+                </ModalFooter>
+              </ModalContent>
+            </Modal> */}
+  
+          </Card>
+            ))
+          )
+        }
+        
 
 
-            </SimpleGrid>
-        </div>
-    )
+      </SimpleGrid>
+    </div>
+  )
 }
 
 export default Project2
