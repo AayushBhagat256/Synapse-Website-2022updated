32776d3aaf99d322109d35626c87da4eb5152ab8
Mansi2210 <mansiprajapati2203@gmail.com>
2023-07-17 12:54:41 +0530

    navbar changes

--- SynapseSite2023/src/Components/Navbar.jsx
+++ SynapseSite2023/src/Components/Navbar.jsx
@@ -1,45 +1,47 @@
 import React, { useState } from 'react'
 import './Navbar.css'
-import logo from './synapselogo.png';
+import logo from '../assets/synapselogo.png';
 import { LuAlignJustify } from "react-icons/lu";
+import { LuChevronDown } from "react-icons/lu";
 import { RxCross2 } from "react-icons/rx";
-import { Link } from 'react-router-dom';
 
-function Navbar() {
-    const [Open, setOpen] = useState(false)
-    const [Mobile, setMobile] = useState(false)
-    return (
-        <>
-            <nav className='navbar'>
-                <div className='container'>
-                    <img src={logo} alt='logo' className='logo'></img>
-                    <ul className={Mobile ? 'mobilelinks' : 'links'} >
-                        <li>Home</li>
-                        <li onClick={() => setOpen(!Open)}> Teams
-                            {
-                                Open && (
-                                    <div className='dropdowndiv'>
-                                        <ul className='dropdown'>
-                                            <li><a href="/fac"> Faculty</a> </li>
-                                            <li><a href="/core">Core</a> </li>
-                                            <li><a href="/excore">Founders</a></li>
-                                        </ul>
-                                    </div>
-                                )
-                            }
-                        </li>
-                        <li>Projects</li>
-                        <li>Events</li>
-                        <li><a href="/contact">Contact Us</a></li>
-                        {/* <Link to='/contact'>Contact us</Link>   */}
-                        {/* plz dont do anything in this link (from ~Aayush) */}
-                    </ul>
-                    <button className='hamburgericon' onClick={() => setMobile(!Mobile)}>
-                        {Mobile ? <RxCross2 /> : <LuAlignJustify />}
-                    </button>
-                </div>
-            </nav>
-        </>
-    )
+const Navbar1 = () => {
+  const [isNavExpanded, setIsNavExpanded] = useState(false)
+ 
+  return (
+    <div className='navbar'>
+      <nav className="navigation">
+      <img src={logo} alt='logo' className='logo'></img>
+      <button className="hamburger" onClick={() => {
+          setIsNavExpanded(!isNavExpanded);
+        }}>
+        {isNavExpanded ? <RxCross2 /> : <LuAlignJustify />}
+      </button>
+      <div
+        className={
+          isNavExpanded ? "navigation-menu expanded" : "navigation-menu"
+        }>
+        <ul className='links'>
+          <li>
+            <a href="/home">Home</a>
+          </li>
+          <li>
+            <a href="/home">Team</a>
+          </li>
+          <li>
+            <a href="/home">Events</a>
+          </li>
+          <li>
+            <a href="/about">Projects</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    </div>
+  )
 }
-export default Navbar;
+
+export default Navbar1
